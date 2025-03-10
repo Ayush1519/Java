9c52e788fb64820012b31e08912a2c938a0eb3f9
Ayush1519 <singhayush6872@gmail.com>
2025-03-10 15:26:03 +0000

    Adding some codes

--- Basics/Basics.java
+++ Basics/Basics.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Welcome to my java world");
+        int a=2;
+        int b=4;
+        int sum=a+b;
+        System.out.println(sum);
     }
 }
