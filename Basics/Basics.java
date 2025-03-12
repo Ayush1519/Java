c6b6f78790cb0b5201335223cf4b43415d9c509d
Ayush1519 <singhayush6872@gmail.com>
2025-03-12 04:39:13 +0000

    Using switch case to print the day

--- Basics/Basics.java
+++ Basics/Basics.java
@@ -15,6 +15,27 @@ public class Main{
                 System.out.println("Zero");
             }
         }
+
+        int day=scanner.nextInt();
+
+        Switch(day) {
+            case 1:System.out.println("Monday");
+            break;
+            case 2:System.out.println("Tuesday");
+            break;
+            case 3:System.out.println("Wednesday");
+            break;
+            case 4:System.out.println("Thursday");
+            break;
+            case 5:System.out.println("Friday");
+            break;
+            case 6:System.out.println("Saterday");
+            break;
+            case 7:System.out.println("Sunday");
+            break;
+            default:System.out.println("Invalid day")");
+
+        }
         
         System.out.println("Hello World");
         System.out.println("Welcome to my java world");
