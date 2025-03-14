1f198e75ca259753756823c34ab08becf846a407
Ayush1519 <singhayush6872@gmail.com>
2025-03-14 07:23:19 +0000

    using some basic odesx of java

---
+++ Basics/loops.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        int n=4;
+
+        for(int i=0; i<=n; i++){
+            System.out.println("*");
+        }
+
+        
+    }
+}
