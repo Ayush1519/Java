f8ce68b40ca60b42a3e1cc29325b186e5ce50dc0
Ayush1519 <singhayush6872@gmail.com>
2025-03-14 07:52:15 +0000

    using for loop to print pattern

--- Basics/loops.java
+++ Basics/loops.java
@@ -21,5 +21,12 @@ public class Main{
             }
             System.out.println();
         }
+
+         for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
