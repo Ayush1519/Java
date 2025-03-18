42b50d8da72df7f3f2a6fe9d4e91877237281091
Ayush1519 <singhayush6872@gmail.com>
2025-03-18 12:44:30 +0000

    Comments

--- Basics/Practice.java
+++ Basics/Practice.java
@@ -1,36 +1,28 @@
-// import java.util.Scanner;
-
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.print("Enter a number: ");
-//         int num = scanner.nextInt();
-//         int originalNum = num;
-//         int reversedNum = 0;
-
-
-//         while (num != 0) {
-//             int digit = num % 10;
-//             reversedNum = reversedNum * 10 + digit;
-//             num = num / 10;
-//         }
-        
-//         if (originalNum == reversedNum) {
-//             System.out.println(originalNum + " is a palindrome.");
-//         } else {
-//             System.out.println(originalNum + " is not a palindrome.");
-//         }
-
-//         scanner.close();
-//     }
-// }
-
 import java.util.Scanner;
 
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Printing the number in reverse and checking if it is a palindrome or not
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int originalNum = num;
+        int reversedNum = 0;
+
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num = num / 10;
+        }
+        
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+
+        //writing a java program to print the greatest number in the given numbers
 		int x=scanner.nextInt();
 		int y=scanner.nextInt();
         int z=scanner.nextInt();
