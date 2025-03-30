427c3f0bce568bfd8d931e897cd72312c27ad568
Ayush1519 <singhayush6872@gmail.com>
2025-03-30 09:45:54 +0000

    learned some new ways of coding

--- Basics/Practice.java
+++ Basics/Practice.java
@@ -183,6 +183,28 @@ public class Main {
 
         System.out.println("\nmobile 2 Details:");
         mobile2.displayInfo();
+
+        int positiveCount = 0, negativeCount = 0, zeroCount = 0;
+        char choice;
+        do {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+
+            if (number > 0) {
+                positiveCount++;
+            } else if (number < 0) {
+                negativeCount++;
+            } else {
+                zeroCount++;
+            }
+
+            System.out.print("Do you want to enter another number? (y/n): ");
+            choice = scanner.next().charAt(0);
+        } while (choice == 'y' || choice == 'Y');
+
+        System.out.println("Count of Positive Numbers: " + positiveCount);
+        System.out.println("Count of Negative Numbers: " + negativeCount);
+        System.out.println("Count of Zeros: " + zeroCount);
       
 	}
 }
