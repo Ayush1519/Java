8e009e573d3330ed017741f32a6a526e84868e8a
Ayush1519 <singhayush6872@gmail.com>
2025-04-03 14:10:03 +0000

    Making a Zoo management system

---
+++ Basics/Inheritence.java
@@ -0,0 +1,55 @@
+class Animal {
+    String name;
+    String type;
+
+    public Animal(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public void displayInfo() {
+        System.out.println("Animal Name: " + name);
+        System.out.println("Type: " + type);
+    }
+}
+
+class Mammal extends Animal {
+    String sound;
+
+    public Mammal(String name, String type, String sound) {
+        super(name, type);
+        this.sound = sound;
+    }
+
+    public void displayMammalInfo() {
+        displayInfo();
+        System.out.println("Sound: " + sound);
+    }
+}
+
+class Bird extends Animal {
+    String flyingAbility;
+
+    public Bird(String name, String type, String flyingAbility) {
+        super(name, type);
+        this.flyingAbility = flyingAbility;
+    }
+
+    public void displayBirdInfo() {
+        displayInfo();
+        System.out.println("Flying Ability: " + flyingAbility);
+    }
+}
+
+public class ZooManagementSystem {
+    public static void main(String[] args) {
+        Mammal lion = new Mammal("Lion", "Carnivore", "Roar");
+        Bird parrot = new Bird("Parrot", "Herbivore", "Can Fly");
+
+        System.out.println("Mammal Information:");
+        lion.displayMammalInfo();
+
+        System.out.println("\nBird Information:");
+        parrot.displayBirdInfo();
+    }
+}
