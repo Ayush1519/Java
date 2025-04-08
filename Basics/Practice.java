1a8b4cac1317d4f33262b1be283605cfe8774c3e
Ayush1519 <singhayush6872@gmail.com>
2025-04-08 13:05:30 +0000

    comments

--- Basics/Practice.java
+++ Basics/Practice.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 class Calculator {
+    // Method to add two integers
     public int add(int a, int b) {
         return a + b;
     }
 
+    // Method to add three integers
     public int add(int a, int b, int c) {
         return a + b + c;
     }
 
+    // Method to add two double values
     public double add(double a, double b) {
         return a + b;
     }
@@ -294,9 +297,9 @@ public class Main {
 
         Calculator calc = new Calculator();
 
-        System.out.println("Sum of two integers: " + calc.add(5, 10));      
-        System.out.println("Sum of three integers: " + calc.add(5, 10, 15));
-        System.out.println("Sum of two doubles: " + calc.add(5.5, 10.5));    
+        System.out.println("Sum of two integers: " + calc.add(5, 10));       // Calls add(int, int)
+        System.out.println("Sum of three integers: " + calc.add(5, 10, 15)); // Calls add(int, int, int)
+        System.out.println("Sum of two doubles: " + calc.add(5.5, 10.5));    // Calls add(double, double)
 
 	}
 }
