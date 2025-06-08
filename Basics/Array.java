ec3c601e6529359fe6a7f5bbef1214c6c5f2be6f
Ayush1519 <singhayush6872@gmail.com>
2025-06-08 02:54:10 +0000

    sum of array

--- Basics/Array.java
+++ Basics/Array.java
@@ -1,8 +1,14 @@
 class Main{
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr1 = {1, 2, 3, 4, 5};
         System.out.println(arr[2]);
 
+        int[] arr2 = {1, 2, 3, 4, 5};
+        int sum =0;
+        for(int i=0; i<arr1.length; i++){
+            sum += arr1[i] + arr2[i];
+        }
+        System.out.println("sum:"+sum);
     }
 }
 
