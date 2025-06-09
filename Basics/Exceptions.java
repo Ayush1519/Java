b1f929b6ffc9588e627e20dad3c1cc1719cbe89a
Ayush1519 <singhayush6872@gmail.com>
2025-06-09 14:04:12 +0000

    Exceptions in java

---
+++ Basics/Exceptions.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try{
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		int division=(a/b);
+		int num = 10 / 0;
+        int arr[] = new int[5];
+        arr[10] = 50; 
+		System.out.println(division);
+		} catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Array index is out of bounds.");
+        } catch (Exception e) {
+            System.out.println("General exception occurred.");
+        }
+        System.out.println("Program continues...");
+
+		
+		System.out.println("Work is done");
+	}
+}
