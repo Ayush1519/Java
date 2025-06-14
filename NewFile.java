a673fe0e07fcf274f27a29647b9312c4e4177ae3
Ayush1519 <singhayush6872@gmail.com>
2025-06-14 15:42:15 +0000

    comments

--- NewFile.java
+++ NewFile.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 class FileEx{
     public static void main(String[] args) throws IOException {
-        File f = new File("Test.txt");
+        File f = new File("Test.txt");//creates new file named as Test.txt
         if(f.createNewFile()) {
             System.out.println("File created");
         } else{
