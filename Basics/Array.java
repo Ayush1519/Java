0c45cd8a126fa723c50cae4a796310feecfe760c
Ayush1519 <singhayush6872@gmail.com>
2025-06-18 03:13:55 +0000

    comments

--- Basics/Array.java
+++ Basics/Array.java
@@ -14,13 +14,13 @@ class Main{
         int[][] mul = new int[arr1.length][arr2.length];
         for(int i=0; i<arr1.length; i++) {
             for(int j=0;j<arr2.length; j++) {
-                mul[i][j]=arr1[i]*arr2[j];
+                mul[i][j]=arr1[i]*arr2[j];//to multiply array 1 and 2
             }
         }
         
         for(int i=0; i<mul.length; i++) {
             for(int j=0;j<mul.length; j++) {
-                System.out.println(mul[i][j]);
+                System.out.println(mul[i][j]);to print the multiplication matrix
             }
         }
     }
