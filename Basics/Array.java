5f9dacf841550d168b5e6fb09cb211d5a6a1e3de
Ayush1519 <singhayush6872@gmail.com>
2025-06-18 03:12:54 +0000

    multiplication matrix

--- Basics/Array.java
+++ Basics/Array.java
@@ -9,6 +9,20 @@ class Main{
             sum += arr1[i] + arr2[i];
         }
         System.out.println("sum:"+sum);
+        int[] arr2 = {1, 2, 3, 4, 5};
+        
+        int[][] mul = new int[arr1.length][arr2.length];
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0;j<arr2.length; j++) {
+                mul[i][j]=arr1[i]*arr2[j];
+            }
+        }
+        
+        for(int i=0; i<mul.length; i++) {
+            for(int j=0;j<mul.length; j++) {
+                System.out.println(mul[i][j]);
+            }
+        }
     }
 }
 
