6d3183413a9f4d9217d9aa2e4b46603526855f2d
Ayush1519 <singhayush6872@gmail.com>
2025-07-25 22:04:23 +0530

    comments

--- Basics/Code.java
+++ Basics/Code.java
@@ -1,7 +1,7 @@
 class Main {
     public static boolean isValidPassword(String password) {
         if (password.length() < 8) {// using password.length to print the length
-            return false;
+            return false;//using return keyword
         }
         char firstChar = password.charAt(0);//char variable declared
         return !(firstChar >= '0' && firstChar <= '9');
