e42da69cb45caa836ec5e0bf949e9ac8ec2b4568
Ayush1519 <singhayush6872@gmail.com>
2025-08-31 14:01:59 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -3,5 +3,15 @@ public class Main {
   public static void main(String[] args) {
     int[] num = new int[]{1, 2, 3, 4, 5};
     System.out.println(num[2]); // Output: 3
+
+    int[][] num = {{1,2},{3,4}};
+        
+        for(int i=0; i<num.length; i++) {
+            for(int j=0; j<num[i].length; j++) {
+                System.out.print(num[i][j]+" ");
+            }
+        }
+        System.out.println();
+    
   }
 }
