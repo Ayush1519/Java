658ea2fa1263d3d90861aa15e701f1a89778ffdd
Ayush1519 <singhayush6872@gmail.com>
2025-09-03 15:59:33 +0530

    Update Inheritence.java

--- Basics/Inheritence.java
+++ Basics/Inheritence.java
@@ -212,3 +212,33 @@ public class Main
 		a1.makeSound();
 	}
 }
+
+//inheritence using extends keyword
+class Animal{
+    void eat() {
+        System.out.println("Animal eats food");
+    }
+}
+
+class Mammal extends Animal{
+    void breathe() {
+        System.out.println("Mammal breathes air");
+    }
+}
+
+class Dog extends Mammal {
+    void bark() {
+        System.out.println("Dog barks.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+
+        
+        myDog.eat();     
+        myDog.breathe(); 
+        myDog.bark();    
+    }
+}
