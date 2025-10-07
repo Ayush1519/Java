3839262757b90ecf61d05fad21f1b270db071e89
Ayush1519 <singhayush6872@gmail.com>
2025-10-07 22:00:32 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Aydev{
+  public static void main (String [] ar) {
+    ArrayList<String> li = new ArrayList<>(); 
+    li.add("Java"); 
+    li.add("Python"); 
+    li.add("C++"); 
+    System.out.println(li);
+    
+    li.remove(1); 
+    System.out.println(li);
+  }   
+}
