6a7594fb0ce3b334213e43f7ee50f9bdc4bfa7a3
Ayush1519 <singhayush6872@gmail.com>
2025-10-23 20:47:08 +0530

    Implement Account class with deposit and withdraw methods

---
+++ Basics/Encapsulation.java
@@ -0,0 +1,34 @@
+class Account {
+    private double balance = 1000;
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Successfully deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount: " + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn amount: " + amount);
+        } else {
+            System.out.println("Insufficient balance for withdrawal: " + amount);
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Account obj = new Account();
+        obj.deposit(3000);
+        obj.withdraw(1000);
+        System.out.println("My present balance = " + obj.getBalance());
+    }
+}
