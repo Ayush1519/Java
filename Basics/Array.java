708dd6bbeb7041f66560e17113ef9ae3a36a204d
Ayush1519 <singhayush6872@gmail.com>
2025-11-09 20:05:55 +0530

    comments

--- Basics/Array.java
+++ Basics/Array.java
@@ -10,7 +10,7 @@ class Main{
         for(int i=0; i<arr1.length; i++){
             sum += arr1[i] + arr2[i];
         }
-        System.out.println("sum:"+sum);
+        System.out.println("sum:"+sum);//printing the output
 
         int[] arr2 = {1, 2, 3, 4, 5};
         int sum =0;
