dd1b9b17c4f80a933a708b65c2e0341d4d24669f
Ayush1519 <singhayush6872@gmail.com>
2025-11-12 20:58:04 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main {
   public static void main(String[] args) {
-    int[] num = new int[]{1, 2, 3, 4, 5};
+    int[] num = new int[]{1, 2, 3, 4, 5};//array declared
     System.out.println(num[2]); // Output: 3
 
     int[][] num = {{1,2},{3,4}};
