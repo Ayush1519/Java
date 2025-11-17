3295a6f718f66def8bb9407de0d4e4f09495d4d1
Ayush1519 <singhayush6872@gmail.com>
2025-11-17 18:31:29 +0530

    Update Longest common prefix.java

--- Longest common prefix.java
+++ Longest common prefix.java
@@ -8,7 +8,7 @@ class Solution {
     for (int i = 0; i < strs[0].length(); i++){//using for loop
         for (int j = 1; j < strs.size(); j++){
         if (i == strs[j].length() || strs[j][i] != strs[0][i])
-          return strs[0].substr(0, i);
+          return strs[0].substr(0, i);//using return keyword
 
       }
     }    
