67202c846b1670bc66450b2f37708069221fbf22
Ayush1519 <singhayush6872@gmail.com>
2025-11-28 22:36:19 +0530

    some changes to correct the flow of code

--- Arrays.java
+++ Arrays.java
@@ -1,19 +1,20 @@
 import java.util.*;
 public class Main {
   public static void main(String[] args) {
-    int[] num = new int[]{1, 2, 3, 4, 5};//array declared
-    System.out.println(num[2]); // Output: 3
+    int[] arr = {1, 2, 3, 4, 5};//array declared
+    System.out.println(arr[2]); // Output: 3
 
-    int[][] num = {{1,2},{3,4}};//array declared
-        
-        for(int i=0; i<num.length; i++) {//using for loop
-            for(int j=0; j<num[i].length; j++) {
-                System.out.print(num[i][j]+" ");
+    int[][] matrix = {{1,2},{3,4}};//array declared
+        for(int i=0; i<matrix.length; i++) {//using for loop
+            for(int j=0; j<matrix[i].length; j++) {
+                System.out.print(matrix[i][j]+" ");
             }
         }
         System.out.println();//used for new line
 
-    int matrix1[3][3], matrix2[3][3], sum[3][3];//matrix declared
+    int matrix1[3][3] = new int[3][3];
+    int matrix2[3][3] = new int[3][3];
+    int sum[3][3] = new int[3][3];//matrix declared
     
     
     
