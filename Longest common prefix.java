e927f65fe6ce509b108c107c8d0279f61b18439d
Ayush1519 <singhayush6872@gmail.com>
2026-02-10 23:20:47 +0530

    Fix formatting and comments in Longest common prefix

--- Longest common prefix.java
+++ Longest common prefix.java
@@ -7,7 +7,7 @@ class Solution {//class declared
 
     for (int i = 0; i < strs[0].length(); i++){//using for loop
         for (int j = 1; j < strs.size(); j++){//using for loop
-        if (i == strs[j].length() || strs[j][i] != strs[0][i])
+        if (i == strs[j].length() || strs[j][i] != strs[0][i])//conditional
           return strs[0].substr(0, i);//using return keyword
 
       }
