2bcd2c52272d0c889b59bdd8697fa6da5e39fa59
Ayush1519 <singhayush6872@gmail.com>
2026-02-15 23:21:09 +0530

    Clarify return statement for empty input
    Added a comment to clarify the return statement for empty input.

--- Longest common prefix.java
+++ Longest common prefix.java
@@ -3,7 +3,7 @@ class Solution {//class declared
  public:
   string longestCommonPrefix(vector<string>& strs) {//function declared
     if (strs.empty())//used if control statement to check if strs is empty
-      return "";
+      return "";//returns empty string
 
     for (int i = 0; i < strs[0].length(); i++){//using for loop
         for (int j = 1; j < strs.size(); j++){//using for loop
